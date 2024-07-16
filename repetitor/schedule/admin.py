b919d301bccffc6b3b8a404e10f5616480212144
Evgenii Gusev <ged1994@mail.ru>
2024-07-16 15:19:18 +0300

    fixed linting errors

--- repetitor/schedule/admin.py
+++ repetitor/schedule/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Schedule, Appointment
-
+from .models import Appointment, Schedule
 
 admin.site.register(Schedule)
 admin.site.register(Appointment)
